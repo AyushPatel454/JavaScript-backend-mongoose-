1e745b961144363c4db8857943706edc6c7f6934
Aayush Patel <iaayush@atyantik.com>
2024-05-27 17:51:25 +0530

    Ecommerce website data modelling / data schema.

---
+++ models/ecommerce/user.models.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true } // createdAt, updatedAt (automatically generated)
+);
+
+export const User = mongoose.model("User", userSchema);
